32141568eb3bb795dc0b9fe2325a69051ec27152
Jeon-Myung-Hyun <46843278+Myung-Hyun@users.noreply.github.com>
2020-09-28 16:13:51 +0900

    Docs : tip about template class

--- Template/function_template.cpp
+++ Template/function_template.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+/* Tip
+template class와 operation이 한 파일로 있어야 actual parameter를 집어넣어 코드 만들기 용이, cpp로 독립시키지 않는다.
+compiler가 template으로부터 class를 만들어 주기 때문 
+*/
+
 template <class DATATYPE> //함수 템플릿, overloading 하지 않고 한 번만 정의 하면 된다. 
 //반환 형식만으로 구분되는 overloading은 할 수 없다.
 DATATYPE abs_Tpl(DATATYPE num)
